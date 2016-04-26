202e6f51f957fc12135248df11ebaf544efc5c12
Alan Penner <apenner@gmail.com>
2016-04-26 08:29:58 -0700

    Make the switch work.

--- devicetypes/penner42/directv-genie-switch.src/directv-genie-switch.groovy
+++ devicetypes/penner42/directv-genie-switch.src/directv-genie-switch.groovy
@@ -15,19 +15,64 @@
  */
 metadata {
 	definition (name: "DirecTV Genie Switch", namespace: "penner42", author: "Alan Penner") {
+		capability "Actuator"
+        capability "Switch"
+//        capability "Momentary"
+//        capability "Sensor"
+        
+        attribute "receiver", "string"
+        attribute "command", "string"
+        attribute "channel", "string"
 	}
 
 	simulator {
 		// TODO: define status and reply messages here
 	}
 
-	tiles {
-		// TODO: define your main and details tiles here
-	}
+	tiles (scale: 2) {
+	    multiAttributeTile(name:"switch", type: "generic", width: 6, height: 4, canChangeIcon: true){
+			tileAttribute ("device.switch", key: "PRIMARY_CONTROL") {
+				attributeState "on", label:'Push', action:"switch.on", icon:"st.lights.philips.hue-single", backgroundColor:"#ffffff"
+//				attributeState "on",  label:'Push', action:"momentary.push", icon:"st.lights.philips.hue-multi"
+			}
+		}
+    }
+	main "switch"
+    details(["switch"])
 }
 
 // parse events into attributes
 def parse(String description) {
 	log.debug "Parsing '${description}'"
 
+}
+
+def push() {
+	log.debug("push")
+	def channel = this.device.currentValue("channel")
+	def receiver = this.device.currentValue("receiver")    
+	def command = this.device.currentValue("command").toLowerCase()
+
+/*	sendEvent(name: "switch", value: "on", isStateChange: true, display: false)
+	sendEvent(name: "switch", value: "off", isStateChange: true, display: false)
+	sendEvent(name: "momentary", value: "pushed", isStateChange: true)    
+  */  
+    switch(command) {
+		case "channel": 
+        	parent.setChannel(receiver, channel)
+            break
+		case "pause":
+        	log.debug "pause"
+			parent.runCommand(receiver, command)
+            break
+	}
+    
+}
+
+def on() {
+	push()
+}
+
+def off() {
+	push()
 }
